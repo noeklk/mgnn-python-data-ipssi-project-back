0cb1d73693085872ace53978907d14c739c799de
Noé <noe.abdelkalek@gmail.com>
2020-11-09 12:25:41 +0100

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ RUN apk update \
     postgresql-dev \
     libpq
 WORKDIR /src/
+RUN pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
+
 ENV PYTHONUNBUFFERED 1
+
 COPY . .
--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,18 @@
-CREATE USER docker;
-CREATE DATABASE docker;
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+-- CREATE USER docker;
+-- CREATE DATABASE docker;
+-- GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+-- psql -c "USE docker";
+
+
+CREATE TABLE utilisateur
+(
+    id INT PRIMARY KEY NOT NULL,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    email VARCHAR(255),
+    date_naissance DATE,
+    pays VARCHAR(255),
+    ville VARCHAR(255),
+    code_postal VARCHAR(5),
+    nombre_achat INT
+)

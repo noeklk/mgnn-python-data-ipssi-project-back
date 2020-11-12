d04c86206b1a3cdffe5e649984386945b2f05a17
Noe ABDEL KALEK <noe.abdelkalek@rentacar.fr>
2020-11-12 11:52:20 +0100

    fix 4_1, add 4_2_1 et 4_2_2

--- src/script/questions.py
+++ src/script/questions.py
@@ -6,7 +6,7 @@ def json_to_dict(json_response, query):
     return jsonify({'result': [dict(row) for row in json_response], 'query': query})
 
 def question2():
-    question2_query = """SELECT (
+    question_2_query = """SELECT (
         SELECT COUNT(*)
         FROM   airlines
         ) AS airlines_count,
@@ -29,31 +29,57 @@ def question2():
         FROM DUAL;"""
 
     with engine.connect() as con:
-        result = con.execute(question2_query).fetchall()
-        return json_to_dict(result, question2_query)
+        result_2 = con.execute(question_2_query).fetchall()
+
+        return json_to_dict(result_2, question_2_query)
 
 def question3():
-    question3_query = """SELECT COUNT(DISTINCT tzone) FROM airports WHERE dst = 'N' AND tzone LIKE '%%America%%';"""
+    question_3_query = """SELECT COUNT(DISTINCT tzone) FROM airports WHERE dst = 'N' AND tzone LIKE '%%America%%';"""
     with engine.connect() as con:
-        result = con.execute(question3_query).fetchall()
-        return json_to_dict(result, question3_query)
+        result = con.execute(question_3_query).fetchall()
+
+        return json_to_dict(result, question_3_query)
 
 def question4():
-    question4_1_query = """select * from
-    (select origin, count(origin), air_p.name AS airport_name
+    question_4_1_query = """select * from
+    (select origin AS airport_origin_faa, count(*) AS flights_count, air_p.name AS airport_name
     from flights
     JOIN airports air_p WHERE air_p.faa = flights.origin
     group by origin
-    order by origin ASC) derived_table
+    order by COUNT(origin) DESC) derived_table
     LIMIT 1"""
 
+    question_4_2_1_query = """select * from
+    (select dest AS airport_destination_faa, count(*) AS flights_count, air_p.name AS airport_name, air_p.tzone AS destination_tzone
+    from flights
+    JOIN airports air_p WHERE air_p.faa = flights.dest
+    group by dest
+    order by COUNT(dest) DESC) derived_table
+    LIMIT 10"""
+
+    question_4_2_2_query = """select * from
+    (select dest AS airport_destination_faa, count(*) AS flights_count, air_p.name AS airport_name, air_p.tzone AS destination_tzone
+    from flights
+    JOIN airports air_p WHERE air_p.faa = flights.dest
+    group by dest
+    order by COUNT(dest) ASC) derived_table
+    LIMIT 10"""
+
     with engine.connect() as con:
-        most_popular_airport_origin = con.execute(question4_1_query).fetchall()
+        result_4_1 = con.execute(question_4_1_query).fetchall()
+        result_4_2_1 = con.execute(question_4_2_1_query).fetchall()
+        result_4_2_2 = con.execute(question_4_2_2_query).fetchall()
 
         json = jsonify(
             {
-                'result_most_popular_airport_origin': [dict(row) for row in most_popular_airport_origin], 
-                'query_most_popular_airport_origin': question4_1_query
+                'result_4_1': [dict(row) for row in result_4_1], 
+                'query_4_1': question_4_1_query,
+
+                'result_4_2_1_most': [dict(row) for row in result_4_2_1],
+                'query_4_2_1_most': question_4_2_1_query,
+
+                'result_4_2_2_least': [dict(row) for row in result_4_2_2],
+                'query_4_2_2_least': question_4_2_2_query,
             })
 
         return json

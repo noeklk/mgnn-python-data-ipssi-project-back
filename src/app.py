162c9ef47d42cac56acf95ccdba2d26ad3367bb4
Noe ABDEL KALEK <noe.abdelkalek@rentacar.fr>
2020-11-13 11:28:35 +0100

    affichage des questions index

--- src/app.py
+++ src/app.py
@@ -14,9 +14,20 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 engine = create_engine('mysql+pymysql://root@localhost:8081/airport-analytics?charset=utf8mb4')
 
+questions_count = 5
+
+def get_routes():
+    url_array = []
+    for i in range(questions_count):
+        n = i+2
+        print(n)
+        url_array.append([f"return_question{n}", n])
+
+    return url_array
+
 @app.route('/')
 def view_index():   
-    return render_template('index.html')
+    return render_template('index.html', routes = get_routes())
 
 @app.route('/api/question-2')
 def return_question2():
